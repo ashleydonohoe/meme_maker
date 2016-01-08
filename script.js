ddd8db2c6c5a3850b3923be9e8e73998bd6513db
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-01-07 21:26:43 -0500

    grabbed text and placed in variables

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 $(document).ready(function() {
 	$('#generate').click(function() {
-		var topText =;
-		var bottomText =;
+		var topText = $('input[name=top-text]').val();
+		var bottomText = $('input[name=bottom-text]').val();
+		console.log(topText);
+		console.log(bottomText);
+		$('.meme-image').append('<h1 class="text-center">' + topText + '</h1>');
+		$('.meme-image').append('<h1 class="text-center">' + bottomText + '</h1>');
 	});
-	$('#clear').click(function() {
-
-	});
-
 });
