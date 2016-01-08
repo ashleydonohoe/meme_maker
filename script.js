471febd905634e49cc4c8df02c24f1d0984171e9
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-01-07 22:01:17 -0500

    has basic functionality

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
 	$('#generate').click(function() {
+		var memeURL = $('input[name=image-url]').val();
 		var topText = $('input[name=top-text]').val();
 		var bottomText = $('input[name=bottom-text]').val();
+		console.log(memeURL);
 		console.log(topText);
 		console.log(bottomText);
-		$('.meme-image').append('<h1 class="text-center">' + topText + '</h1>');
-		$('.meme-image').append('<h1 class="text-center">' + bottomText + '</h1>');
+		$('.meme-image').css('background-image', 'url(' + memeURL + ')');
+		$('.meme-image').append('<h1 class="text-center meme-top">' + topText + '</h1>');
+		$('.meme-image').append('<h1 class="text-center meme-bottom">' + bottomText + '</h1>');
+	});
+	$('#clear').click(function() {
+		$('.meme-image').remove();
+
+	});
+	/* Taken from http://stackoverflow.com/questions/4078245/onsubmit-return-false-has-no-effect-on-internet-explorer-7-8-form-is-still-su */
+	$("form[name=myform]").bind('submit',function(){
+   		return false;
 	});
 });
+
